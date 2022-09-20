4e3e0683d5cc9d9acf6056a50a49d31637e6474f
guilhermecosta10 <94014302+guilhermecosta10@users.noreply.github.com>
2022-09-20 16:00:04 -0300

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -5,6 +5,23 @@ class Game
     this.torneioClassico = createButton("Torneio Clássico");
     this.torneioNacional = createButton("Torneio Nacional");
     this.torneioInternacional = createButton("Torneio Internacional");
+
+    this.Pele = createElement("h3");
+    this.Cabelo = createElement("h3")
+    this.Olho = createElement("h3");
+
+    this.p1 = createButton("");
+    this.p2 = createButton("");
+    this.p3 = createButton("");
+
+    this.c1 = createButton("");
+    this.c2 = createButton("");
+    this.c3 = createButton("");
+
+    this.o1 = createButton("");
+    this.o2 = createButton("");
+    this.o3 = createButton("");
+
   }
 
   //tela inicial de cadastro do jogador
@@ -75,7 +92,52 @@ class Game
     cabelo.shapeColor = "#180b02"
 
 
+    this.p1.position(600,350);
+    this.p1.class("p1");
 
+    this.p2.position(600,450);
+    this.p2.class("p2");
+
+    this.p3.position(600,550);
+    this.p3.class("p3");
+
+
+
+    this.c1.position(1040,350);
+    this.c1.class("c1");
+
+    this.c2.position(1040,450);
+    this.c2.class("c2");
+
+    this.c3.position(1040,550);
+    this.c3.class("c3");
+
+
+
+    this.o1.position(1530,350);
+    this.o1.class("o1");
+
+    this.o2.position(1530,450);
+    this.o2.class("o2");
+
+    this.o3.position(1530,550);
+    this.o3.class("o3");
+
+
+
+    this.Pele.position(600,150);
+    this.Pele.class("leadersText");
+    this.Pele.html("Pele");
+
+    this.Cabelo.position(1000,150);
+    this.Cabelo.class("leadersText");
+    this.Cabelo.html("Cabelo")
+
+    this.Olho.position(1500,150);
+    this.Olho.class("leadersText");
+    this.Olho.html("Olhos");
+
+    this.detectPele(pele);
 
     drawSprites();
   }
@@ -122,11 +184,30 @@ class Game
   {
     this.torneioInternacional.mousePressed(()=>{
 
-     this.esconderBotoesTorneio();
-     tipoDeCampeonato = 3;
+    this.esconderBotoesTorneio();
+    tipoDeCampeonato = 3;
       
     })
   }
+
+  detectPele(p) 
+  {
+    this.p1.mousePressed(()=>{
+
+      console.log("apertou p1");
+      p.shapeColor = "#e6b870";
+    })
+
+    this.p2.mousePressed(()=>{
+
+      pele.shapeColor = "#503b0f";
+    })
+
+    this.p3.mousePressed(()=>{
+
+      pele.shapeColor = "#180e02";
+    })
+ }
 
 
   esconderBotoesTorneio() 
